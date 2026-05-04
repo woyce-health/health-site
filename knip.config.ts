3190d5cbd62f90064e2c833e73db00d24e0ecf3f
Shyamsinh Parmar <parmarshyamsingh8@gmail.com>
2026-05-04 12:28:27 +0530

    Initial commit

---
+++ knip.config.ts
@@ -0,0 +1,23 @@
+import type { KnipConfig } from 'knip';
+
+const config: KnipConfig = {
+  // Files to exclude from Knip analysis
+  ignore: ['checkly.config.ts', 'src/libs/I18n.ts', 'src/types/I18n.ts', 'tests/**/*.ts'],
+  // Dependencies to ignore during analysis
+  ignoreDependencies: [
+    '@commitlint/types',
+    '@clerk/shared',
+    '@swc/helpers', // Avoid error in CI: "`npm ci` can only install packages when your package.json and package-lock.json or npm-shrinkwrap.json are in sync."
+    'postcss',
+    'vite',
+  ],
+  // Binaries to ignore during analysis
+  ignoreBinaries: [
+    'production', // False positive raised with dotenv-cli
+  ],
+  compilers: {
+    css: (text: string) => [...text.matchAll(/(?<=@)import[^;]+/g)].join('\n'),
+  },
+};
+
+export default config;
---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,26 @@
+import { enUS, frFR } from '@clerk/localizations';
+import type { LocalizationResource } from '@clerk/shared/types';
+import type { LocalePrefixMode } from 'next-intl/routing';
+
+/** Locale prefix strategy for next-intl routing. */
+const localePrefix: LocalePrefixMode = 'as-needed';
+
+// FIXME: Update this configuration file based on your project information
+export const AppConfig = {
+  name: 'Nextjs Starter',
+  i18n: {
+    locales: ['en', 'fr'],
+    defaultLocale: 'en',
+    localePrefix,
+  },
+};
+
+const supportedLocales: Record<string, LocalizationResource> = {
+  en: enUS,
+  fr: frFR,
+};
+
+export const ClerkLocalizations = {
+  defaultLocale: enUS,
+  supportedLocales,
+};
